3211558695b8347cce9d74a3e03246fe2463913d
MadhushaPrasad <madushaprasad21@gmail.com>
2022-03-18 12:49:18 +0530

    feat(layout):create BirthdaBook,New,Update and list activities

---
+++ app/src/main/java/com/example/memory_bus/birthday/NewBirthday.java
@@ -0,0 +1,16 @@
+package com.example.memory_bus.birthday;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.memory_bus.R;
+
+public class NewBirthday extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_new_birthday);
+    }
+}
