b541ef4dac267acf6addd40a2a9831e221d446a3
MadhushaPrasad <madushaprasad21@gmail.com>
2022-03-18 12:45:34 +0530

    feat(layout):create recipeBook,New,Update and list activities

---
+++ app/src/main/java/com/example/memory_bus/recipe/RecipeBook.java
@@ -0,0 +1,16 @@
+package com.example.memory_bus.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.memory_bus.R;
+
+public class RecipeBook extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_recipe_book);
+    }
+}
---
+++ app/src/main/java/com/example/memory_bus/recipe/UpdateRecipe.java
@@ -0,0 +1,16 @@
+package com.example.memory_bus.recipe;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.memory_bus.R;
+
+public class UpdateRecipe extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_update_recipe);
+    }
+}
